f90ffdb1a36ba23be4721c135ada6567f5a31940
chingjyu <69333371+chingjyu@users.noreply.github.com>
2025-10-27 11:30:24 +0800

    retry

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -46,7 +46,8 @@ UPDATE_PACKAGE() {
 
 # UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
 UPDATE_PACKAGE "argon" "chingjyu/luci-theme-argon" "main"
-UPDATE_PACKAGE "sing-box" "reF1nd/sing-box" "reF1nd-main" "name"
+
+UPDATE_PACKAGE "sing-box" "reF1nd/sing-box" "reF1nd-main"
 UPDATE_PACKAGE "homeproxy" "immortalwrt/homeproxy" "master"
 UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
 UPDATE_PACKAGE "momo" "nikkinikki-org/OpenWrt-momo" "main"
@@ -106,6 +107,7 @@ UPDATE_VERSION() {
 # Temprarily unavailable
 # UPDATE_VERSION "tailscale"
 # UPDATE_VERSION "sing-box"
+
 
 
 
