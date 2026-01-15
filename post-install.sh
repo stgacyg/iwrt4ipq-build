c2c1559bf7246d0f33ffb704a8e6997bc5f711be
Victor Chan <69333371+chingjyu@users.noreply.github.com>
2026-01-15 16:33:42 +0800

    update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -5,9 +5,9 @@ wget -O - https://github.com/nikkinikki-org/OpenWrt-momo/raw/refs/heads/main/fee
 # 下载v2ray geodata
 apk add v2ray-geosite v2ray-geoip
 # 删除xray-core
-/etc/init.d/xray stop
 /etc/init.d/xray disable
 rm /usr/bin/xray
+rm /etc/init.d/xray
 # 删除xray残留文件
 rm -r /etc/xray
 rm -r /usr/share/xray
