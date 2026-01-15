49059df0532331c1670bfd9a0f7197a4236a4fc9
Victor Chan <69333371+chingjyu@users.noreply.github.com>
2026-01-15 16:18:41 +0800

    update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,8 +1,11 @@
 # 配置软件源
 sed -i 's|downloads\.immortalwrt\.org|immortalwrt.kyarucloud.moe|g' /etc/apk/repositories.d/distfeeds.list
-# 删除xray-core
-rm /usr/bin/xray
 # 添加OpenWrt-momo软件源
 wget -O - https://github.com/nikkinikki-org/OpenWrt-momo/raw/refs/heads/main/feed.sh | ash
-# 安装v2ray geodata
+# 下载v2ray geodata
 apk add v2ray-geosite v2ray-geoip
+# 删除xray-core
+rm /usr/bin/xray
+# 删除xray残留文件
+rm -r /etc/xray
+rm -r /usr/share/xray
